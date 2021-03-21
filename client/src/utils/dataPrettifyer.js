49b40b8beeab58f733d6db59a5b7e1e6da8a72b7
yamnyk <notAunicorN2018>
2021-03-21 10:47:07 +0200

    fix date prettifyer

--- client/src/utils/dataPrettifyer.js
+++ client/src/utils/dataPrettifyer.js
@@ -1,6 +1,8 @@
 const dataPrettifyer = (dateStr, divider = ".") => {
   const date = new Date(dateStr);
-  return `${date.getDay()}${divider}${date.getMonth()}${divider}${date.getFullYear()}`;
+  return `${date.getDate()}${divider}${
+    date.getMonth() + 1
+  }${divider}${date.getFullYear()}`;
 };
 
 export default dataPrettifyer;
