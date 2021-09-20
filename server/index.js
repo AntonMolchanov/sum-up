867f12603c340594fc27e6990de510bdce1ae396
Nik Yamnyk <38811365+yamnyk@users.noreply.github.com>
2021-09-20 10:29:53 +0300

    add pleasures route to the app

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import express, { json, urlencoded } from "express";
+import express, {json, urlencoded} from "express";
 import path from "path";
 import mongoose from "mongoose";
 
@@ -6,16 +6,18 @@ import config from "./config.js";
 import days from "./routes/days.route.js";
 import users from "./routes/user.route.js";
 import situations from "./routes/situations.route.js";
+import pleasures from "./routes/pleasures.route.js";
 import auth from "./utils/auth.js";
 
 const app = express();
 
 app.use(json());
-app.use(urlencoded({ extended: false }));
+app.use(urlencoded({extended: false}));
 app.use(express.static(path.resolve(path.dirname("")) + "/public/"));
 app.use("*", auth);
 
 app.use("/api/days", days);
+app.use("/api/pleasures", pleasures);
 app.use("/api/situations", situations);
 app.use("/api/users", users);
 
